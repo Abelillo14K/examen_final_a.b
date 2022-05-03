05d2c9bb286e4b991cfa8a801732679ab5cd65b3
Abel de Leon <100392460+Abelillo14K@users.noreply.github.com>
2022-05-03 10:10:17 -0600

    cambios, de programa

--- main.py
+++ main.py
@@ -2,4 +2,14 @@ print("Nombre: Abel Alexander de Leon Lima")
 print("codigo: 1572322")
 
 
-ab = ("que animal es tu favortio:")
+print("hola, bienvenido, cual es tu animal favorito, quieres saber cual es el mio!!!\n")
+
+
+# MI ANIMAL FAVORITO ES: Leon 
+print("Animal favorito clave: Leon\n")
+animal = input("Por favor, ingresa tu animal favorito: ")
+
+if animal == "Leon":
+    print("A mí también me gustan los Leon, veo que te gustan los felinos, son muy bonitos y rudos!!!")
+else:
+    print(f"Es un animal muy interesante el {animal}, pero a mi me gusta mas los leones .")
