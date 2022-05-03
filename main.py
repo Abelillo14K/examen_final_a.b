7e88bf3051ffcfac2dfe2ca72755ee0d6d425416
Abel de Leon <100392460+Abelillo14K@users.noreply.github.com>
2022-05-03 10:03:55 -0600

    ingreso de codigo

--- main.py
+++ main.py
@@ -2,7 +2,17 @@ print("Nombre: Abel Alexander de Leon Lima")
 print("codigo: 1572322")
 
 
-ab = int("que animal es tu favortio:")
+print("ingresa tu animal favortio, y ¡¡ Y CUAL CREES QUE ES MI ANIMAL FAVORITO!!!\n")
+
+
+# MI ANIMAL FAVORITO ES: leon 
+print("Animal favorito clave: leon\n")
+animal = input("Ingresa tu animal favorito: ")
+
+if animal == "leon":
+    print("A mí también me gustan los leones, sin dua alguna, el mejor felino de la naturaleza!!!")
+else:
+    print(f"ya veo, igual es un animal muy interesante {animal} pero yo prefiero los leones.")
 
 
 
